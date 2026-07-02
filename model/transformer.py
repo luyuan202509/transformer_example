22e1fdcb9a26950ee6fcf37c90badcd2fddaf4cd
luyuan <luyuan202509@gmail.com>
2026-07-02 22:04:53 +0800

    模型搭建

--- model/transformer.py
+++ model/transformer.py
@@ -1,13 +1,13 @@
 from this import d
+from numpy.matlib import number
 import torch 
+from torch._dynamo import source
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np 
 from common import Encoder,Decoder,Genarator,Embedding,FNN
-from common import EncoderLayer,DecoderLayer,MultiHeadAttention
-
-
-
+from common import EncoderLayer,DecoderLayer,MultiHeadAttention,PositionalEncoding
+import copy 
 
 class EncoderDecoder(nn.Module):
     def __init__(self,encoder:Encoder,decoder:Decoder,source_embed:Embedding,
@@ -31,6 +31,37 @@ class EncoderDecoder(nn.Module):
     def decode(self,memory:torch.Tensor,src_mask:torch.Tensor,target:torch.Tensor,target_mask:torch.Tensor):
         
         return self.decoder(self.target_embd(target),memory,src_mask,target_mask)
+
+
+
+def make_model(source_vocab_size:int,target_vocab_size:int,num_layer:int=6,
+               embed_dim:int=512,num_head:int = 8,dropout_rate:int = 0.1):
+    
+    cp = copy.deepcopy
+
+    # 获取多头注意力
+    attn = MultiHeadAttention(num_head,embed_dim,dropout_rate)
+
+    # 实例化前馈网络
+    ffn = FNN(embed_dim,embed_dim,embed_dim)
+
+    # 实例化位置编码
+    pos_encode = PositionalEncoding(embed_dim,dropout_rate)
+    
+    encoder = Encoder(EncoderLayer(embed_dim,cp(attn),cp(ffn),dropout_rate),num_layer)
+    decoder = Decoder(DecoderLayer(embed_dim,cp(attn),cp(attn),cp(ffn),dropout_rate),num_layer)
+    src_emb = Embedding(source_vocab_size,embed_dim) 
+    target_emb = Embedding(target_vocab_size,embed_dim)
+    source_embed = nn.Sequential(src_emb,cp(pos_encode))
+    target_embed = nn.Sequential(target_emb,cp(pos_encode))
+    generator = Genarator(embed_dim,target_vocab_size)
+
+    model = EncoderDecoder(encoder,decoder,source_embed,target_embed,generator)
+    for p in model.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform_(p)
+    return model
+
 
 
 def main():
@@ -61,9 +92,12 @@ def main():
     print(ed_result)
     print(ed_result.shape)
 
-    
+
+def create_model():
+    source_vocab_size = target_vocab_size = 11
+    num_layer =6
+    model = make_model(source_vocab_size,target_vocab_size,num_layer)
+    print(model)
 
 if __name__ == "__main__":
-    main()
-    
-
+   create_model()
